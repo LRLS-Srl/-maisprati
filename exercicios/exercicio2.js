cedc918874e9f8d618c542971fbca4886c5c3acd
Leonardo Sales <leonardsales126@gmail.com>
2025-05-05 20:59:01 -0300

    atividade 2 arquivo

---
+++ exercicios/exercicio2.js
@@ -0,0 +1,6 @@
+/* 2. Crie um programa que classifica a idade de uma pessoa em categorias (criança,
+adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de
+controle if-else. 
+
+Para executar o projeto utilize no prompt: node exercicio2.js
+*/
