86c80ce8f7025b89784d3073b16bbc2ec5343d2d
Leonardo Sales <leonardsales126@gmail.com>
2025-05-08 21:36:29 -0300

    Exercicio 2

--- exercicios/exercicio2.js
+++ exercicios/exercicio2.js
@@ -1,6 +1,12 @@
-/* 2. Crie um programa que classifica a idade de uma pessoa em categorias (criança,
-adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de
-controle if-else. 
+// 2. Crie um programa que classifica a idade de uma pessoa em categorias (criança,
+// adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de
+// controle if-else.const prompt = require("prompt-sync")({ sigint: true });
 
-Para executar o projeto utilize no prompt: node exercicio2.js
-*/
+function ex2() {
+  const idade = parseInt(prompt("Exercicio2 - Digite a idade: "));
+  if (idade <= 12) console.log("Criança");
+  else if (idade <= 17) console.log("Adolescente");
+  else if (idade <= 59) console.log("Adulto");
+  else console.log("Idoso");
+}
+module.exports = ex2;
