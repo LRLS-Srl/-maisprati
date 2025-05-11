dfdb81852027a04982c5f1932cdb01d538c7770b
Leonardo Sales <leonardsales126@gmail.com>
2025-05-11 20:14:22 -0300

    Anotações sobre javascript

---
+++ anotações..js
@@ -0,0 +1,6 @@
+///Anotações extras sobre o curso e boas praticas nas escritas em java script
+
+/* console.log ele envia a mensagem para o console do navegador.
+As mensagens devem estar entre os parenteses e as  mensagens dentro deles podem estar entre aspas simples
+e aspas duplas , e o ponto e virgula é considerado uma boa prática entre os devs para evitar erros nos códigos
+*/
