78cb3b16d17bb0140042d2cba711c749514381bd
Leonardo Sales <leonardsales126@gmail.com>
2025-05-12 09:51:57 -0300

    Testes

--- anotações..js
+++ anotações..js
@@ -4,3 +4,34 @@
 As mensagens devem estar entre os parenteses e as  mensagens dentro deles podem estar entre aspas simples
 e aspas duplas , e o ponto e virgula é considerado uma boa prática entre os devs para evitar erros nos códigos
 */
+
+
+// EX. 1 Nome de exibição
+
+console.log("Nome: Leonardo");
+
+//Ex.2 árvore de natal
+
+console.log("   a   ");
+console.log("  b c  ");
+console.log(" d e f ");
+console.log("g h i j");
+console.log("   k   "); 
+
+/* Para fazermos comentarios de várias linhas usamos barra lateral e asterisco /* e termina o comentario com asterisco e barra dianteira
+nós permitindo fazer diversas linhas de comentarios.
+*/
+
+// Desafio Final feito no Codedex Fazer uma nota fiscal com console.log
+
+console.log("================================================");
+ console.log("                   Posto Gambino               ");
+ console.log("------------------------------------------------");
+ console.log("               Detalhes de compra              ");
+ console.log("------------------------------------------------");
+ console.log("     Item         Quantidade        Valor      ");
+ console.log("------------------------------------------------");
+ console.log("Gasolina Comum       12LT        R$ 70,00");
+ console.log("-------------------------------------------------");
+
+ //Forma divertida de aprender a fazer uma string.
