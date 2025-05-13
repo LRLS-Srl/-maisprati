b3f30f416f7a068b864caf30f035aee24ae568c9
Leonardo Sales <leonardsales126@gmail.com>
2025-05-12 21:10:47 -0300

    Controle com if  e else

---
+++ controledefluxo.js
@@ -0,0 +1,10 @@
+// Condicionais if e else com if ele válida se a informação é true e executa o código se for verdadeira
+// se não for verdaddeira ela apenas pula oque está após o if e nada acontece
+
+//Ex.1 fazendo um impressor de bom dia caso a hora seja menor que 12H
+
+const hora = 11; // minha const tem que ser menor que 12h para imprimir Bom dia no console
+
+if(hora < 12) {
+  console.log("Bom dia");
+}
